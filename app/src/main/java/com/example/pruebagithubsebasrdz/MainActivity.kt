aada020e8d5b1c17e788e055d85c9a66689e23a8
SebasROCS <sebasrdz88@gmail.com>
2024-09-06 13:34:09 -0600

    WSTA ES UNA PRUEBAf

--- app/src/main/java/com/example/pruebagithubsebasrdz/MainActivity.kt
+++ app/src/main/java/com/example/pruebagithubsebasrdz/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PruebaGitHubSebasRdzTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android",//ola
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
